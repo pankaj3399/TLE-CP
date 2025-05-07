75df7fb78db9a6956850aaea384c94a60eaf2b99
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2025-05-08 00:52:53 +0530

    Problems

---
+++ Level1/K_Max_and_Min.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define fi first
+#define se second
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define mii map<int, int>
+#define msi map<string, int>
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << min(a, min(b, c)) << " " << max(a, max(b, c)) << "\n";
+    return 0;
+}
