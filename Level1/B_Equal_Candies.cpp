93fa20f3a5b20516d0e4437faa2394f93a00eb78
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2025-05-16 23:43:22 +0530

    Problems

---
+++ Level1/B_Equal_Candies.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define fi first
+#define se second
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define mii map<int, int>
+#define msi map<string, int>
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++){
+            cin>> arr[i];
+        }
+        sort(arr.begin(),arr.end());
+        ll ans = 0;
+        for(int i=1;i<n;i++){
+            ans+= arr[i]-arr[0];
+        }
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
